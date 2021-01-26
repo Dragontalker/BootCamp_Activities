fdc9f6dcc8533fb8abaabad1897769139c06f3df
Richard Yang <richard.yang.tong@gmail.com>
2021-01-26 16:49:50 -0500

    Completed Task 2.

--- Day_16/CodecademyProject_MadisonSquareMarket/Assets/JavaScripts/script.js
+++ Day_16/CodecademyProject_MadisonSquareMarket/Assets/JavaScripts/script.js
@@ -1,4 +1,21 @@
 // Task 1: Start by using jQuery to add a ready method call to the document.
 $(document).ready(() => {
+    const $cart = $('#cart');
+    const $account = $('#account');
+    const $help = $('#help');
 
+    // Add click event handlers to '#cart'
+    $cart.on('click', () => {
+
+    })
+
+    // Add click event handlers to '#account'
+    $account.on('click', () => {
+
+    })
+
+    // Add click event handlers to '#help'
+    $help.on('click', () => {
+        
+    })
 })
