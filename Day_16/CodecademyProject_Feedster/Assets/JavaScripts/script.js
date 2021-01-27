541e9ca0119913912e6371d0cfb4cbe445f8aff9
Richard Yang <richard.yang.tong@gmail.com>
2021-01-26 21:20:55 -0500

    Completed Task 1.

--- Day_16/CodecademyProject_Feedster/Assets/JavaScripts/script.js
+++ Day_16/CodecademyProject_Feedster/Assets/JavaScripts/script.js
@@ -1,3 +1,6 @@
 $(document).ready(() => {
-
+    // Task 1: add an event handler that shows the .nav-menu element when a user mouse over the .menu element.
+    $('.menu').on('mouseenter', () => {
+        $('.nav-menu').show();
+    })
 }); 
