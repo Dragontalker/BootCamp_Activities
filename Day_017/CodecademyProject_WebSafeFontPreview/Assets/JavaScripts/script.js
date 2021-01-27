df4b0e53892ad646b1bf802eff923e144f8104ad
Richard Yang <richard.yang.tong@gmail.com>
2021-01-27 09:10:56 -0500

    Completed Task 3.

--- Day_017/CodecademyProject_WebSafeFontPreview/Assets/JavaScripts/script.js
+++ Day_017/CodecademyProject_WebSafeFontPreview/Assets/JavaScripts/script.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
-    // task 2: add a keyup event handler to '#text'
+    // Task 2: add a keyup event handler to '#text'
     $('#text').on('keyup', event => {
-        
+        // Task 3: call the html methods on '.preview' element and pass it the current value of current target of event.
+        $('.preview').html($(event.currentTarget).val());
     })
 })
