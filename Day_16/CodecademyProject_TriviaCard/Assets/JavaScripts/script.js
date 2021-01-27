2fd44e25a641fe9f629bc5fc98791c7d299403b2
Richard Yang <richard.yang.tong@gmail.com>
2021-01-26 20:33:56 -0500

    Completed Task 2 and linked the css/js to html file.

--- Day_16/CodecademyProject_TriviaCard/Assets/JavaScripts/script.js
+++ Day_16/CodecademyProject_TriviaCard/Assets/JavaScripts/script.js
@@ -3,6 +3,7 @@ $(document).ready(() => {
     const $hintBox = $('.hint-box');
 
     $hintBox.on('click', () => {
-        
+        // Task 2: when the box is clicked, we want the HTML element to 'hint' class to slide into view
+        $('.hint').slideToggle(650);
     })
 })
