4eb6fb071a752e4f924ba1a146bfb6a4ed684917
Richard Yang <richard.yang.tong@gmail.com>
2021-01-27 09:14:47 -0500

    Completed Task 4.

--- Day_017/CodecademyProject_WebSafeFontPreview/Assets/JavaScripts/script.js
+++ Day_017/CodecademyProject_WebSafeFontPreview/Assets/JavaScripts/script.js
@@ -3,5 +3,8 @@ $(document).ready(() => {
     $('#text').on('keyup', event => {
         // Task 3: call the html methods on '.preview' element and pass it the current value of current target of event.
         $('.preview').html($(event.currentTarget).val());
-    })
+    });
+
+    // Task 4: attach a change event handler to the <select> filed with an id of 'font'.
+    $('#id').change();
 })
