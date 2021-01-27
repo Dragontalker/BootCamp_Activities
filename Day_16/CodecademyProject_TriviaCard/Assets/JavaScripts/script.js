df884fb5d1a2c022c963a5c68d3fd15432219abd
Richard Yang <richard.yang.tong@gmail.com>
2021-01-26 20:31:34 -0500

    Completed Task 1.

---
+++ Day_16/CodecademyProject_TriviaCard/Assets/JavaScripts/script.js
@@ -0,0 +1,8 @@
+// Task 1: target 'hint-box', create event handler for it on 'click'
+$(document).ready(() => {
+    const $hintBox = $('.hint-box');
+
+    $hintBox.on('click', () => {
+        
+    })
+})
