72b6d153e22bd4c36a2f2a03dd2362a33d47f869
Richard Yang <richard.yang.tong@gmail.com>
2021-01-29 16:35:11 -0500

    Completed Task 2.

--- Day_019/CodecademyProject_BuildALibrary/app.js
+++ Day_019/CodecademyProject_BuildALibrary/app.js
@@ -1,4 +1,7 @@
 // Task 1: Create an empty class called Media.
 class media {
-    
+    // Task 2: Create an empty constructor() method that takes on parameter.
+    constructor (title) {
+        this._title = title;
+    }
 }
