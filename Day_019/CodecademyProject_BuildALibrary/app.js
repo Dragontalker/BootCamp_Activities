fd58118c1cfbfa1ec9ac7b4fe3bbb42d91a50e0f
Richard Yang <richard.yang.tong@gmail.com>
2021-01-29 16:36:57 -0500

    Completed with Task 3.

--- Day_019/CodecademyProject_BuildALibrary/app.js
+++ Day_019/CodecademyProject_BuildALibrary/app.js
@@ -2,6 +2,9 @@
 class media {
     // Task 2: Create an empty constructor() method that takes on parameter.
     constructor (title) {
+        // Task 3: Inside the contructor, set the values for the properties that Book, CD, and Movie share.
         this._title = title;
+        this._isCheckedOut = false;
+        this._rating = [];
     }
 }
