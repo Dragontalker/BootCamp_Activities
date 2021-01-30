fca62c33ccf556fc252734afcf7b573f87d974b5
Richard Yang <richard.yang.tong@gmail.com>
2021-01-30 15:37:54 -0500

    Added more notes on module.

--- Day_020/Learning_NodeJS/app.js
+++ Day_020/Learning_NodeJS/app.js
@@ -1,19 +1,4 @@
-function sayHello(name) {
-    console.log('Hello ' + name);
-}
+// Loading log from module logger.js
+var logger = require('./logger');
 
-
-console.log(); // Console is a global object.
-
-// Global objects available both in browers and node
-setTimeout();
-clearTimeout();
-setInterval();
-clearInterval();
-
-// Window and global has different scope
-// In brower, global variables are stored under window object.
-// However, in node, global.message won't work, different scope.
-var message = '';
-
-console.log(global.message);
+logger.log('message');
