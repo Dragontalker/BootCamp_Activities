26e5d0ae4638bcd835d85091d2c51d3b9156a91b
Richard Yang <richard.yang.tong@gmail.com>
2021-01-30 15:31:35 -0500

    Added more notes.

---
+++ Day_020/Learning_NodeJS/logger.js
@@ -0,0 +1,7 @@
+var url = 'http://mylogger.io/log ';
+
+// Only available to scope logger.js, or in another way, private.
+function log(message) {
+    //Send an HTTP request
+    console.log(message);
+}
