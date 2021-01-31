aa3fdad61fd2806b6eb750fbac4061a0d75d1065
Richard Yang <richard.yang.tong@gmail.com>
2021-01-30 19:43:04 -0500

    Added the chaining of then() method.

--- Day_020/CodecademyProject_Promise/app.js
+++ Day_020/CodecademyProject_Promise/app.js
@@ -1,14 +1,22 @@
-const {checkInventory} = require('./library.js');
+const {checkInventory, processPayment, shipOrder} = require('./library.js');
 
-const order = [['sunglasses', 1], ['bags', 2]];
-
-// Write your code below:
-const handleSuccess = (resolvedValue) => {
-  console.log(resolvedValue);
+const order = {
+  items: [['sunglasses', 1], ['bags', 2]],
+  giftcardBalance: 79.82
 };
 
-const handleFailure = (rejectReason) => {
-  console.log(rejectReason);
-};
-
-checkInventory(order).then(handleSuccess).catch(handleFailure);
+checkInventory(order)
+.then((resolvedValueArray) => {
+  // Write the correct return statement here:
+ return processPayment(resolvedValueArray);
+})
+.then((resolvedValueArray) => {
+  // Write the correct return statement here:
+  return shipOrder(resolvedValueArray);
+})
+.then((successMessage) => {
+  console.log(successMessage);
+})
+.catch((errorMessage) => {
+  console.log(errorMessage);
+});
