c9b57e000716595d7f965de80b76fc963ffe5e1f
Richard Yang <richard.yang.tong@gmail.com>
2021-01-30 19:36:42 -0500

    Completed Lesson 4.

--- Day_020/CodecademyProject_Promise/app.js
+++ Day_020/CodecademyProject_Promise/app.js
@@ -23,4 +23,17 @@ const orderSunglasses = () => {
 const orderPromise = orderSunglasses();
 
 // Log the promise, you can see that the promise resolved.
-console.log(orderPromise);                      
+console.log(orderPromise);           
+
+// Demostrating how timeout works.
+console.log("This is the first line of code in app.js.");
+// Keep the line above as the first line of code
+
+const usingSTO = () => {
+  console.log("This line of code will log to the console last.");
+};
+
+setTimeout(usingSTO, 100);
+
+// Keep the line below as the last line of code:
+console.log("This is the last line of code in app.js.");
