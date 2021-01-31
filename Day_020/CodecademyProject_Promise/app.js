543922287986786c15eb9da551d92def32af5bd6
Richard Yang <richard.yang.tong@gmail.com>
2021-01-30 19:40:03 -0500

    Completed Lesson 6.

--- Day_020/CodecademyProject_Promise/app.js
+++ Day_020/CodecademyProject_Promise/app.js
@@ -1,39 +1,15 @@
-// Defining constant object for inventory.
-const inventory = {
-    sunglasses: 1900,
-    pants: 1088,
-    bags: 1344
-};
-  
-// Create an executor function.
-const myExecutor = (resolve, reject) => {
-    if (inventory.sunglasses > 0) {
-        resolve('Sunglasses order processed.');
-    } else {
-        reject('That item is sold out.');
-    }
+const {checkInventory} = require('./library.js');
+
+const order = [['sunglasses', 1], ['bags', 2]];
+
+// Write your code below:
+const handleSuccess = (resolvedValue) => {
+  console.log(resolvedValue);
 };
 
-// Create a function that creates the promise.
-const orderSunglasses = () => {
-    return new Promise(myExecutor);
+const handleFailure = (rejectReason) => {
+  console.log(rejectReason);
 };
 
-// Create the promise.
-const orderPromise = orderSunglasses();
-
-// Log the promise, you can see that the promise resolved.
-console.log(orderPromise);           
-
-// Demostrating how timeout works.
-console.log("This is the first line of code in app.js.");
-// Keep the line above as the first line of code
-
-const usingSTO = () => {
-  console.log("This line of code will log to the console last.");
-};
-
-setTimeout(usingSTO, 100);
-
-// Keep the line below as the last line of code:
-console.log("This is the last line of code in app.js.");
+checkInventory(order)
+  .then(handleSuccess, handleFailure);
