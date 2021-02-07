4fe39663bba7d261029cc11674c0e48414c7e2e2
Richard Yang <richard.yang.tong@gmail.com>
2021-02-07 15:52:42 -0500

    Completed the bonus activity.

--- Month2_BackEnd/Day01_Feb06/M9A8_FunctionalLoops/map.js
+++ Month2_BackEnd/Day01_Feb06/M9A8_FunctionalLoops/map.js
@@ -1,10 +1,10 @@
 const originalArray = [1, 3, 2, 5, 10];
 
 // doubledArray is equal to a new array of numbers multiplied by 2 and returned by map(). map() will use an anonymous function as a condition. 
-const doubledArray = originalArray.map(function(data) {
+const doubledArray = originalArray.map(data => 
   // The anonymous function returns each number multiplied by 2. The returned result is added to a new array.
-  return data * 2;
-});
+  data * 2
+);
 
 console.log(doubledArray);
 console.log(originalArray);
