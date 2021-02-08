3ff8606c10d265ec827aaf2212994f91e9a7d691
Richard Yang <richard.yang.tong@gmail.com>
2021-02-08 15:46:14 -0500

    Completed Task 4.

--- Month2_BackEnd/Day02_Feb07/CodecademyProject_SchoolCatalogue/app.js
+++ Month2_BackEnd/Day02_Feb07/CodecademyProject_SchoolCatalogue/app.js
@@ -6,11 +6,22 @@ class School {
         this._name = name,
         this._level = level,
         this._numberOfStudents = numberOfStudents
+    };
+
+    // Create getters for the name, level and numberOfStudents properties.
+    get name() {
+        return this._name;
+    }
+    get level() {
+        return this._level;
+    }
+    get numberOfStudents() {
+        return this._numberOfStudents;
     }
 };
 
 // Test:
 let alSmith = new School('Al E. Smith', 'highschool', 415);
-console.log(alSmith._name);
-console.log(alSmith._level);
+console.log(alSmith.name);
+console.log(alSmith.level);
 console.log(alSmith._numberOfStudents)
