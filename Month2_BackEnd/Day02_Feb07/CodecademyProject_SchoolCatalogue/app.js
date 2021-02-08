080937e63db71e52d7e1dc83ae6d5c28e2357864
Richard Yang <richard.yang.tong@gmail.com>
2021-02-08 14:27:38 -0500

    Completed Task 2.

--- Month2_BackEnd/Day02_Feb07/CodecademyProject_SchoolCatalogue/app.js
+++ Month2_BackEnd/Day02_Feb07/CodecademyProject_SchoolCatalogue/app.js
@@ -1,4 +1,15 @@
 // Task 1: Create an empty class named School.
-const School {
-    
-}
+class School {
+    // Task 2: Create an empty constructor() that accepts three parameters.
+    constructor (name, level, numberOfStudents) {
+        this._name = name,
+        this._level = level,
+        this._numberOfStudents = numberOfStudents
+    }
+};
+
+// Test:
+let alSmith = new School('Al E. Smith', 'highschool', 415);
+console.log(alSmith._name);
+console.log(alSmith._level);
+console.log(alSmith._numberOfStudents)
