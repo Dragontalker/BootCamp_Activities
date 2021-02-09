807e40c5afb7abd64841b0b7508323a4c5463667
Richard Yang <richard.yang.tong@gmail.com>
2021-02-09 10:35:50 -0500

    Further improved the output the console to make logic more clarified.

--- Month2_BackEnd/Day03_Feb09/M9A16_Maths/index.js
+++ Month2_BackEnd/Day03_Feb09/M9A16_Maths/index.js
@@ -1,18 +1,34 @@
 // TODO: Import `maths.js`
-const Math = require('./maths');
+const maths = require('./maths');
 
 // TODO: Capture the values passed from the command line into these three variables: `operation`, `numOne` and `numTwo`
 const operation = process.argv[2];
-console.log(operation);
+console.log(`The first argument is "${operation}".`);
 
 const numOne = parseInt(process.argv[3]);
-console.log(numOne);
+console.log(`The second argument is "${numOne}".`);
 
 const numTwo = parseInt(process.argv[4]);
-console.log(numTwo);
+console.log(`The third argument is "${numTwo}".`);
 
 // Therefore, when you process in node: node index.js 1 2 3, the console will print 1 2 3.
 
 // TODO: Create a `switch` statement that accepts an `operation` parameter
 // and each `case` uses the corresponding `maths` method
 // to perform each math operation on the two numbers, `numOne` and `numTwo`
+switch (operation) {
+    case 'sum':
+      console.log(`The result is "${maths.sum(numOne, numTwo)}".`);
+      break;
+    case 'difference':
+      console.log(`The result is "${maths.difference(numOne, numTwo)}".`);
+      break;
+    case 'product':
+      console.log(`The result is "${maths.product(numOne, numTwo)}".`);
+      break;
+    case 'quotient':
+      console.log(`The result is "${maths.quotient(numOne, numTwo)}".`);
+      break;
+    default:
+      console.log('Check your maths!');
+}
