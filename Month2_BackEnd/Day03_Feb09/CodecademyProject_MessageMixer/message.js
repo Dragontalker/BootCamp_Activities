8fc59ae93b3dc80621f6157fee0066715b4d8391
Richard Yang <richard.yang.tong@gmail.com>
2021-02-09 09:16:38 -0500

    Completed Task 12.

--- Month2_BackEnd/Day03_Feb09/CodecademyProject_MessageMixer/message.js
+++ Month2_BackEnd/Day03_Feb09/CodecademyProject_MessageMixer/message.js
@@ -1,5 +1,8 @@
 // Task 7: Import the module in message.js using the require() statement.
-const MessageMixer = require('./messageMixer');
+// const MessageMixer = require('./messageMixer');
+
+// Task 12: Use the import keyword to import the MessageMixer module.
+import MessageMixer from './messageMixer';
 
 // Task 2: Coppy the displayMessage() function and the displayMessage() function call and paste it in message.js.
 function displayMessage() {
