0089e5c686d82eb7cfe6e64c2aaa50ddc2f7bd71
Richard Yang <richard.yang.tong@gmail.com>
2021-02-09 10:18:49 -0500

    Completed the activity.

--- Month2_BackEnd/Day03_Feb09/M9A14_AppendFile/index.js
+++ Month2_BackEnd/Day03_Feb09/M9A14_AppendFile/index.js
@@ -1,7 +1,11 @@
 // TODO: What are we importing?
 const fs = require('fs');
+// We are import fs object from another module called fs.
 
 // TODO: Add comments to explain each of the three arguments of appendFile()
+// The first argument takes the name of the file we want to write in;
+// The second argument takes the data we want to write in;
+// The third argument takes any error message that would result from the operation.
 fs.appendFile('log.txt', `${process.argv[2]}\n`, (err) =>
   // TODO: Describe how this ternary operator works
   err ? console.error(err) : console.log('Commit logged!')
