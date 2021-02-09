1415bafc83b03c62d3e34a6fffe86d26b5a4c181
Richard Yang <richard.yang.tong@gmail.com>
2021-02-09 10:25:08 -0500

    Completed the activity.

---
+++ Month2_BackEnd/Day03_Feb09/M9A15_Modularization/badmath.js
@@ -0,0 +1,9 @@
+const pie = ['apple', 'cheery', 'mashmellow'];
+
+const predictable = () => Math.floor(Math.random() * 3);
+
+// module.exports is an object we use to store variables or methods
+module.exports = {
+  pie,
+  predictable,
+};
---
+++ Month2_BackEnd/Day03_Feb09/M9A15_Modularization/index.js
@@ -0,0 +1,9 @@
+const badmath = require('./badmath.js');
+
+let index1 = badmath.predictable();
+let index2 = badmath.predictable();
+let index3 = badmath.predictable();
+
+console.log(badmath.pie[index1]);
+console.log(badmath.pie[index2]);
+console.log(badmath.pie[index3]);
