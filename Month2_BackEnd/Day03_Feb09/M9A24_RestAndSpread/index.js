c4068a2894d3d2db2a0bfa675a08a39d0df38355
Richard Yang <richard.yang.tong@gmail.com>
2021-02-09 12:02:21 -0500

    Completed Exercise 2.

--- Month2_BackEnd/Day03_Feb09/M9A24_RestAndSpread/index.js
+++ Month2_BackEnd/Day03_Feb09/M9A24_RestAndSpread/index.js
@@ -13,18 +13,23 @@ console.log(newSongs);
 const addition = (x, y, z) => {
   const array = [x, y, z];
   // TODO: What does the reduce() method do?
+  // reduce() takes the array and output a single value from it.
   return array.reduce((a, b) => a + b, 0);
 };
 // TODO: What do you expect to be logged in the console?
+// the expected value of output is 6.
 console.log(addition(1, 2, 3));
 
 // TODO: What is this syntax that is being used as the parameter?
+// by using the spread operator, the function can take as many as parameters instead of just 3.
 const additionSpread = (...array) => {
   return array.reduce((a, b) => a + b, 0);
 };
 
 // TODO: What do you expect to be logged in the console?
+// the expected value of output is 6.
 console.log(additionSpread(1, 2, 3));
 
 // TODO: What do you expect to be logged in the console?
+// the expected value of output is 110.
 console.log(additionSpread(1, 2, 3, 4, 100));
