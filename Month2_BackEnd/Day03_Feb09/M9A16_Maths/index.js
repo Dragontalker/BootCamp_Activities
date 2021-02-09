809b988794fde17ae5fd10907dc5cfff938f7044
Richard Yang <richard.yang.tong@gmail.com>
2021-02-09 10:32:04 -0500

    Added the feature that allow console capiture the inputs.

--- Month2_BackEnd/Day03_Feb09/M9A16_Maths/index.js
+++ Month2_BackEnd/Day03_Feb09/M9A16_Maths/index.js
@@ -2,6 +2,16 @@
 const Math = require('./maths');
 
 // TODO: Capture the values passed from the command line into these three variables: `operation`, `numOne` and `numTwo`
+const operation = process.argv[2];
+console.log(operation);
+
+const numOne = parseInt(process.argv[3]);
+console.log(numOne);
+
+const numTwo = parseInt(process.argv[4]);
+console.log(numTwo);
+
+// Therefore, when you process in node: node index.js 1 2 3, the console will print 1 2 3.
 
 // TODO: Create a `switch` statement that accepts an `operation` parameter
 // and each `case` uses the corresponding `maths` method
