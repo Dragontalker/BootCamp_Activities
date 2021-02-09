355c5396445a0ea8fc294c81576ce37ce87ee801
Richard Yang <richard.yang.tong@gmail.com>
2021-02-09 11:53:55 -0500

    Added loop to append new class green to the list.

--- Month2_BackEnd/Day03_Feb09/M9A22_ForOf/index.js
+++ Month2_BackEnd/Day03_Feb09/M9A22_ForOf/index.js
@@ -3,3 +3,10 @@ const songs = document.querySelectorAll("ul > li");
 
 console.log(songs);
 console.log(songs[0].classList);
+
+for (const song of songs) {
+    song.classList.add("green");
+}
+
+// Check if it works:
+console.log(songs[0].classList);
