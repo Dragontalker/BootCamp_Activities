90450b79dbab8e9f8791b95e583b4a1db233add5
Richard Yang <richard.yang.tong@gmail.com>
2021-02-09 08:59:56 -0500

    Completed Task 7.

--- Month2_BackEnd/Day03_Feb09/CodecademyProject_MessageMixer/message.js
+++ Month2_BackEnd/Day03_Feb09/CodecademyProject_MessageMixer/message.js
@@ -1,3 +1,6 @@
+// Task 7: Import the module in message.js using the require() statement.
+const MessageMixer = require('./messageMixer');
+
 // Task 2: Coppy the displayMessage() function and the displayMessage() function call and paste it in message.js.
 function displayMessage() {
     console.log(countCharacter("What is the color of the sky?", "t"));
