d74b95dab8f01319766b14e4beca267d997288e5
Richard Yang <richard.yang.tong@gmail.com>
2021-02-09 09:34:29 -0500

    Completed Task 17.

--- Month2_BackEnd/Day03_Feb09/CodecademyProject_MessageMixer/message.js
+++ Month2_BackEnd/Day03_Feb09/CodecademyProject_MessageMixer/message.js
@@ -20,15 +20,16 @@ import {
 // Task 2: Coppy the displayMessage() function and the displayMessage() function call and paste it in message.js.
 function displayMessage() {
     // Task 8: Call each of the functions as properties of the imported object.
-    console.log(MessageMixer.countCharacter("What is the color of the sky?", "t"));
-    console.log(MessageMixer.capitalizeFirstCharacterOfWords("What is the color of the sky?"));
-    console.log(MessageMixer.reverseWord("What is the color of the sky?"));
-    console.log(MessageMixer.reverseAllWords("What is the color of the sky?"));
-    console.log(MessageMixer.replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
-    console.log(MessageMixer.encode("What is the color of the sky?"));
+    // Task 17: Modify each of the function within each of the displayMessage so that they use only the variable name in the function call.
+    console.log(countCharacter("What is the color of the sky?", "t"));
+    console.log(capitalizeFirstCharacterOfWords("What is the color of the sky?"));
+    console.log(reverseWord("What is the color of the sky?"));
+    console.log(reverseAllWords("What is the color of the sky?"));
+    console.log(replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
+    console.log(Mencode("What is the color of the sky?"));
     // Task 13: Use console.log() to display the output of palindrome()) function. You will need to pass the functions a string.
-    console.log(MessageMixer.palindrome("What is the color of the sky?"));
-    console.log(MessageMixer.pigLatin("What is the color of the sky?", "$"));
+    console.log(palindrome("What is the color of the sky?"));
+    console.log(pigLatin("What is the color of the sky?", "$"));
 }
 
 displayMessage();
