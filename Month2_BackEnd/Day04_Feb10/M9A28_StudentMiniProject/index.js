45b4306f45e6734bd28bb711064fc947eb7e36bb
Richard Yang <richard.yang.tong@gmail.com>
2021-02-10 09:35:27 -0500

    Create a async process for writing file and store it.

--- Month2_BackEnd/Day04_Feb10/M9A28_StudentMiniProject/index.js
+++ Month2_BackEnd/Day04_Feb10/M9A28_StudentMiniProject/index.js
@@ -2,6 +2,8 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 
+const writeFileAsync = util.promisify(fs.writeFile);
+
 const generateHTML = (answers) =>
   `<!DOCTYPE html>
 <html lang="en">
