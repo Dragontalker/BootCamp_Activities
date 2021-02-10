0e4522330f70d40fdd90234eaf19a015987f0e33
Richard Yang <richard.yang.tong@gmail.com>
2021-02-10 09:38:01 -0500

    Moved inquirer call into a function call.

--- Month2_BackEnd/Day04_Feb10/M9A28_StudentMiniProject/index.js
+++ Month2_BackEnd/Day04_Feb10/M9A28_StudentMiniProject/index.js
@@ -4,6 +4,41 @@ const util = require('util');
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
+const promptUser = () =>
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
+    },
+    {
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ]
+);
+
 const generateHTML = (answers) =>
   `<!DOCTYPE html>
 <html lang="en">
