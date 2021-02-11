d8dc45e95fbd354ce666f32c9ef0aad8311f1c94
Richard <richard.yang.tong@gmail.com>
2021-02-11 14:25:27 -0500

    Completed task 2.

--- Month2_BackEnd/Day05_Feb11/M10A08_TDDdissect/index.js
+++ Month2_BackEnd/Day05_Feb11/M10A08_TDDdissect/index.js
@@ -14,3 +14,7 @@ console.log(value);
 
 // Task 1: What do you think will be printed to the console?
 // My guess: the result might be 4+8+15-16-23+42+108=138.
+
+// Task 2: Run the index.js in your terminal, was your guess correct?
+// Yes, executing index.js gives 138, my guess was correct.
+
