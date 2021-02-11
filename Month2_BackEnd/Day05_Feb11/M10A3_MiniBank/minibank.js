6b8747aa5d4de9609f1187de1efb4710166898b3
Richard <richard.yang.tong@gmail.com>
2021-02-11 10:07:11 -0500

    Completed task 1.5.

--- Month2_BackEnd/Day05_Feb11/M10A3_MiniBank/minibank.js
+++ Month2_BackEnd/Day05_Feb11/M10A3_MiniBank/minibank.js
@@ -8,17 +8,25 @@ function MiniBank(balance) {
   // This function should receive a `value` parameter and assign it to the `balance` property of `MiniBank`.
   this.setBalance = (value) => {
     this.balance = value;
-  }
+  };
 
   // Task 1.3:  Write an `updateStatement` function that takes in a number and pushes it to the `statement` array.
   this.updateStatement = (value) => {
     this.statement.push(value);
-  }
+  };
 
   // Task 1.4: Write a `getStatement` function that returns the `statement` property.
   this.getStatement = () => {
     return this.statement;
-  }
+  };
+
+  // Task 1.5: Write a `printStatement` function that prints each element in the `statement` array on its own line.
+  this.printStatement = () => {
+    const statement = this.getStatement();
+    for (let i = 0; i < statement.length; i++) {
+      console.log(`${i+1}. ${statement[i]}`);
+    };
+  };
 
   this.getBalance = () => {
     return this.balance;
