16f2f24f6aa5d9a9c08d756d4dfbbf8e6b7ad3f2
Richard <richard.yang.tong@gmail.com>
2021-02-11 09:41:09 -0500

    Created a constructor function called Developer.

--- Month2_BackEnd/Day05_Feb11/M10A2_Constructors/index.js
+++ Month2_BackEnd/Day05_Feb11/M10A2_Constructors/index.js
@@ -1,6 +1,14 @@
 // TODO: Create a constructor function called 'Developer' that takes in 'name' and 'tech'
+function Developer(name, tech) {
+    this.name = name;
+    this.tech = tech;
+    // TODO: Include a method called 'introduction()' that introduces the Developer with their name and favorite tech
+    this.introduction = () => {
+        console.log(`Hi, my name is ${this.name} and I love ${this.tech}!`);
+    };
+};
 
-// TODO: Include a method called 'introduction()' that introduces the Developer with their name and favorite tech
+
 
 // TODO: Create a new object using the 'Developer' constructor
 
