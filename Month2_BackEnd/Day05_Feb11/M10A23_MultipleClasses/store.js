53b97df8b50c1215b0ed31ef1a3dffd2039794b4
Richard <richard.yang.tong@gmail.com>
2021-02-11 11:48:30 -0500

    Completed the constructor for toy class.

--- Month2_BackEnd/Day05_Feb11/M10A23_MultipleClasses/store.js
+++ Month2_BackEnd/Day05_Feb11/M10A23_MultipleClasses/store.js
@@ -3,27 +3,27 @@ class Store {
     this._name = name;
     this._stock = stock;
     this._revenue = revenue;
-  }
+  };
 
   get name() {
     return this._name;
-  }
+  };
 
   get stock() {
     return this._stock;
-  }
+  };
 
   get revenue() {
     return this._revenue;
-  }
+  };
 
   printRevenue() {
     console.log(`The revenue so far is ${this.revenue}`);
-  }
+  };
 
   welcome() {
     console.log(`Welcome to ${this.name}!`);
-  }
+  };
 }
 
 // Test: 
--- Month2_BackEnd/Day05_Feb11/M10A23_MultipleClasses/toy.js
+++ Month2_BackEnd/Day05_Feb11/M10A23_MultipleClasses/toy.js
@@ -1,4 +1,22 @@
-class Toy {}
+class Toy {
+  constructor(name, price, count) {
+    this._name = name;
+    this._price = price;
+    this._count = count;
+  };
+
+  get name() {
+    return this._name;
+  };
+
+  get price() {
+    return this._price;
+  };
+
+  get count() {
+    return this._count;
+  };
+};
 
 const toys = [
   new Toy("Action Figure", 14.99, 5),
