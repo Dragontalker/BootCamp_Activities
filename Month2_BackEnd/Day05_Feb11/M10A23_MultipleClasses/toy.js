6a4d0105799203053623957545d5a6acfdb4df5a
Richard <richard.yang.tong@gmail.com>
2021-02-11 11:51:05 -0500

    Added the second test case.

--- Month2_BackEnd/Day05_Feb11/M10A23_MultipleClasses/toy.js
+++ Month2_BackEnd/Day05_Feb11/M10A23_MultipleClasses/toy.js
@@ -24,6 +24,12 @@ console.log(actionFigure.name);
 console.log(actionFigure.price);
 console.log(actionFigure.count);
 
+// Test 2:
+const rareToy = new Toy('Rare Toy', 17.99, 1);
+console.log(rareToy.name);
+console.log(rareToy.price);
+console.log(rareToy.count);
+
 const toys = [
   new Toy("Action Figure", 14.99, 5),
   new Toy("Rare Toy", 17.99, 1)
