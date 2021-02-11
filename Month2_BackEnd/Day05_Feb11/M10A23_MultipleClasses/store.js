8e7f06b4b7fa92fe962fd72aa740a0d1887c6dee
Richard <richard.yang.tong@gmail.com>
2021-02-11 11:42:38 -0500

    Created new constructor for store class and associated getter method.

--- Month2_BackEnd/Day05_Feb11/M10A23_MultipleClasses/store.js
+++ Month2_BackEnd/Day05_Feb11/M10A23_MultipleClasses/store.js
@@ -1,5 +1,21 @@
 class Store {
-  constructor() {}
+  constructor(name, stock, revenue) {
+    this._name = name;
+    this._stock = stock;
+    this._revenue = revnue;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get stock() {
+    return this._stock;
+  }
+
+  get revenue() {
+    return this._revenue;
+  }
 
   printRevenue() {
     console.log(`The revenue so far is ${this.revenue}`);
