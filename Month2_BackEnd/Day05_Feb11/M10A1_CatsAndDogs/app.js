92f216942615964972debc51b618444f8375e2ba
Richard <richard.yang.tong@gmail.com>
2021-02-11 09:29:47 -0500

    Added a method called makeNoise to both objects.

--- Month2_BackEnd/Day05_Feb11/M10A1_CatsAndDogs/app.js
+++ Month2_BackEnd/Day05_Feb11/M10A1_CatsAndDogs/app.js
@@ -1,11 +1,21 @@
 // Create a dog object.
 const dog ={
     raining: true,
-    noise: "Woof!"
+    noise: "Woof!",
+    makeNoise() {
+        if (this.raining) {
+            console.log(this.noise);
+        }
+    }
 };
 
 // Create a cat object.
 const cat = {
     raining: false,
-    noise: "Meow!"
+    noise: "Meow!",
+    makeNoise() {
+        if (this.raining) {
+            console.log(this.noise);
+        }
+    }
 };
