d543a48ba4c00579657aefea31769088aea1835a
Richard <richard.yang.tong@gmail.com>
2021-02-12 13:32:06 -0500

    Added a reject action in promise call.

---
+++ Month2_BackEnd/Day06_Feb12/M10A10_PromiseAll/index.js
@@ -0,0 +1,21 @@
+const apiCallDurations = [3000, 4000, 5000, 6000];
+const maxDuration = 9999;
+
+const callAPI = (duration) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // TODO: If the duration is longer than maxDuration, reject() the promise
+      if (duration >= maxDuration) {
+        reject(new Error(`This request has timed out after ${duration}ms`));
+      }
+      // TODO: Otherwise resolve() the promise
+    }, duration);
+  });
+
+const promises = [];
+
+apiCallDurations.map((duration) => promises.push(callAPI(duration)));
+
+console.log('Promises array before the timeouts have finished: ', promises);
+
+// TODO: Use Promise.all() to capture when the array of promises has been resolved or if any of them were rejected
