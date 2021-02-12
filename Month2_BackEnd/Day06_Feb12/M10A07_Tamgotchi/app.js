53c496bd0bdc0fdb6ebe129c068b20ccca2cf691
Richard <richard.yang.tong@gmail.com>
2021-02-12 13:05:21 -0500

    Added a new method, feed.

--- Month2_BackEnd/Day06_Feb12/M10A07_Tamgotchi/app.js
+++ Month2_BackEnd/Day06_Feb12/M10A07_Tamgotchi/app.js
@@ -6,3 +6,16 @@ const DigitalPal = function() {
     this.age = 0;
     this.houseQuality = 100;
   };
+
+  // Prototype method: feed.
+  DigitalPal.prototype.feed = function() {
+    if (this.hungry) {
+      console.log("That was yummy!");
+      this.hungry = false;
+      this.sleepy = true;
+    }
+    else {
+      console.log("No thanks, I'm full.");
+    }
+  };
+  
