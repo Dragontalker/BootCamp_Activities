8f28df9c33999cf0e93bb4635fecaf0c7aa58e19
Richard <richard.yang.tong@gmail.com>
2021-02-12 11:51:56 -0500

    Created the constructor function.

---
+++ Month2_BackEnd/Day06_Feb12/M10A06_RPGprototypes/app.js
@@ -0,0 +1,8 @@
+// Constructor function:
+function Character(name, profession, age, strength, hitpoitns) {
+    this.name = name;
+    this.profession = profession;
+    this.age = age;
+    this.strength = strength;
+    this.hitpoints = hitpoints;
+}
