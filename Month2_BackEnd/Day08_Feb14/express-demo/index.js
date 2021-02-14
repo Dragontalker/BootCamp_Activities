f78e05d738b97d7be852d730b267ff01bb2d3ab1
Richard <richard.yang.tong@gmail.com>
2021-02-14 11:31:51 -0500

    Defined the very firt route for our request.

--- Month2_BackEnd/Day08_Feb14/express-demo/index.js
+++ Month2_BackEnd/Day08_Feb14/express-demo/index.js
@@ -4,4 +4,7 @@ const app = express();
 
 // First arguement: url, where '/' represents roots of the folder
 // Second argument : a callback function with (request, response)
-app.get('/', (req, res));
+app.get('/', (req, res) => {
+    // Defining the very first route
+    res.send('Hello World');
+});
