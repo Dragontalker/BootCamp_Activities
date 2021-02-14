da1fc2cf04823157a63418931a653f96f236cdd9
Richard <richard.yang.tong@gmail.com>
2021-02-14 18:08:35 -0500

    Fixed bugs and improved syntax.

--- Month2_BackEnd/Day08_Feb14/express-demo/index.js
+++ Month2_BackEnd/Day08_Feb14/express-demo/index.js
@@ -76,18 +76,14 @@ app.put('api/courses/:id', (req, res) => {
     // Look up the course
     // If not existing, return 404
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given ID was not found.');
+    if (!course) return res.status(404).send('The course with the given ID was not found.');
 
     // Validate
     // If validate, return 400 - Bad request
     // Use object destrucuring
     const {error} = validateCourse(req.body);
 
-    if (error) {
-        // 400 Bad Request
-        res.status(400).send(error.details[0].message);
-        return;
-    };
+    if (error) return res.status(400).send(error.details[0].message);
 
     // Update course 
     course.name = req.body.name;
@@ -110,7 +106,7 @@ app.delete('/api/courses/:id', (req, res) => {
     // Look up the course
     // Not existing, return 404
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given ID was not found.');
+    if (!course) return res.status(404).send('The course with the given ID was not found.');
 
     // Delete
     const index = courses.indexOf(course);
