993d564c58279d623673e5e3ce321b063ee7a29b
Richard <richard.yang.tong@gmail.com>
2021-02-14 11:33:10 -0500

    Added a port listener at 3000.

--- Month2_BackEnd/Day08_Feb14/express-demo/index.js
+++ Month2_BackEnd/Day08_Feb14/express-demo/index.js
@@ -5,6 +5,9 @@ const app = express();
 // First arguement: url, where '/' represents roots of the folder
 // Second argument : a callback function with (request, response)
 app.get('/', (req, res) => {
-    // Defining the very first route
+    // Defining the very first route, this is also called a route handler
     res.send('Hello World');
 });
+
+// Add a port listenr
+app.listen(3000, () => console.log('Listening on PORT 3000....'));
