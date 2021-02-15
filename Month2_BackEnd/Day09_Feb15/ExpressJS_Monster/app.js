82388b71f3d12645a3b8ca28ee0f8a71bb5791cb
Richard <richard.yang.tong@gmail.com>
2021-02-15 10:29:25 -0500

    Improved the structure of monsters and added a get handler for each monster.

--- Month2_BackEnd/Day09_Feb15/ExpressJS_Monster/app.js
+++ Month2_BackEnd/Day09_Feb15/ExpressJS_Monster/app.js
@@ -3,21 +3,16 @@ const app = express();
 
 const PORT = process.env.PORT || 4001;
 
-const monsters = [
-    {
-        type: 'werewolf'
-    },
-    {
-        type: 'hydra'
-    },
-    {
-        type: 'chupacabra'
-    }
-];
+const monsters = { hydra: { height: 3, age: 4 }, dragon: { height: 200, age: 350 } };
 
 app.get('/monsters', (req, res, next) => {
     res.send(monsters);
-})
+});
+
+app.get('/monsters/:name', (req, res, next) => {
+    console.log(req.params)
+    res.send(monsters[req.params.name]);
+});
 
 
 app.listen(PORT, () => {
