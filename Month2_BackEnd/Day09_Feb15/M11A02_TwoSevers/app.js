28f2aef53905b5bd726313c06bdaa362a98cf258
Richard <richard.yang.tong@gmail.com>
2021-02-15 10:59:18 -0500

    Added the first server.

--- Month2_BackEnd/Day09_Feb15/M11A02_TwoSevers/app.js
+++ Month2_BackEnd/Day09_Feb15/M11A02_TwoSevers/app.js
@@ -1,2 +1,14 @@
 const http = require('http');
 
+// Creating the first server.
+const PORT1 = 7000;
+
+const callbackFunction1 = (request, response) => {
+    response.end('To err is human, but to really foul things up you need a computer');
+};
+
+const serverOne = http.createServer(callbackFunction1);
+
+serverOne.listen(PORT1, () => {
+    console.log(`Server litstening on : http://localhost:${PORT1}...`);
+});
