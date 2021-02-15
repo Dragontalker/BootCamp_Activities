364b26265a2bf4d049f9d84983d7a511bd2f7030
Richard <richard.yang.tong@gmail.com>
2021-02-15 10:23:31 -0500

    Added a get handler.

--- Month2_BackEnd/Day09_Feb15/ExpressJS_Monster/app.js
+++ Month2_BackEnd/Day09_Feb15/ExpressJS_Monster/app.js
@@ -2,6 +2,24 @@ const express = require('express');
 const app = express();
 
 const PORT = 4001;
+
+const monsters = [
+    {
+        type: 'werewolf'
+    },
+    {
+        type: 'hydra'
+    },
+    {
+        type: 'chupacabra'
+    }
+];
+
+app.get('/monsters', (req, res, next) => {
+    res.send(monsters);
+})
+
+
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}...`);
 });
