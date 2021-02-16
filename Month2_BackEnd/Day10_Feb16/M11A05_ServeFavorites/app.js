cee07f47b48ea285b41824d56e0a09d523ed5842
Richard <richard.yang.tong@gmail.com>
2021-02-16 10:03:41 -0500

    Created a handler request for index.html.

--- Month2_BackEnd/Day10_Feb16/M11A05_ServeFavorites/app.js
+++ Month2_BackEnd/Day10_Feb16/M11A05_ServeFavorites/app.js
@@ -2,3 +2,15 @@ const http = require('http');
 const fs = require('fs');
 
 const PORT = process.env.PORT || 8080;
+
+const handleRequestHome = (req, res) => {
+    const path = req.url;
+    
+    if (path === "/index") {
+        return fs.readFile(`${__dirname}/${path}.html`, (err, data) => {
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.send(data);
+        });
+    };
+};
