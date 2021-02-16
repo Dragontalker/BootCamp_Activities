04b5b2c5dd2daafadc986723455e1046aa9976d9
Richard <richard.yang.tong@gmail.com>
2021-02-16 10:54:33 -0500

    Added a demonstration that code directly into jest.

--- Month2_BackEnd/Day10_Feb16/LearningJestJS_TraversyMedia/functions.js
+++ Month2_BackEnd/Day10_Feb16/LearningJestJS_TraversyMedia/functions.js
@@ -1,5 +1,12 @@
 const functions = {
     add: (num1, num2) => num1 + num2,
+    isNull: () => null,
+    checkValue: (value) => value,
+    createUser: () => {
+        const user = {firstName: 'Brad'};
+        user['lastName'] = 'Travery';
+        return user;
+    }
 };
 
 module.exports = functions;
--- Month2_BackEnd/Day10_Feb16/LearningJestJS_TraversyMedia/functions.test.js
+++ Month2_BackEnd/Day10_Feb16/LearningJestJS_TraversyMedia/functions.test.js
@@ -4,9 +4,33 @@ const functions = require('./functions');
 test('Adds 2 + 2 equal 4', () => {
     // Using the basic Jest Matcher, toBe()
     expect(functions.add(2, 2)).toBe(4);
-})
+});
 
 test('Adds 2 + 2 not equal 5', () => {
     // Using not.toBe()
     expect(functions.add(2, 2)).not.toBe(5);
+});
+
+test('Should be NULL', () => {
+    // Testing falsy using toBeNull()
+    expect(functions.isNull(2, 2)).toBeNull();
+});
+
+test('Should be FALSY', () => {
+    // Testing falsy using toBeFalsy()
+    expect(functions.checkValue(null)).toBeFalsy();
+});
+
+test('User should be Brad Traversy object', () => {
+    // toBe() is only for primitive type, we need toEqual() here
+    expect(functions.createUser()).toEqual({firstName: 'Brad', lastName: 'Travery'});
+});
+
+// Coding directing into jest:
+test('Should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 700;
+    const load3 = 800;
+    expect(load1 + load2).toBeLessThan(1600);
+    expect(load1 + load3).toBeLessThanOrEqual(1600);
 })
