bee4f95b59bf8fb57cd6b5a489c869ff44909626
Richard <richard.yang.tong@gmail.com>
2021-02-16 10:41:18 -0500

    Added another test to show not.toBe().

--- Month2_BackEnd/Day10_Feb16/LearningJestJS_TraversyMedia/functions.test.js
+++ Month2_BackEnd/Day10_Feb16/LearningJestJS_TraversyMedia/functions.test.js
@@ -1,1 +1,12 @@
+const { TestScheduler } = require('jest');
+const functions = require('./functions');
 
+test('Adds 2 + 2 equal 4', () => {
+    // Using the basic Jest Matcher, toBe()
+    expect(functions.add(2, 2)).toBe(4);
+})
+
+test('Adds 2 + 2 not equal 5', () => {
+    // Using not.toBe()
+    expect(functions.add(2, 2)).not.toBe(5);
+})
