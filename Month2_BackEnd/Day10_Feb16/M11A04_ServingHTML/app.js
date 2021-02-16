5cfcda79087e32792355b612fc6f7a75772aac89
Richard <richard.yang.tong@gmail.com>
2021-02-16 09:43:02 -0500

    Replaced express with regular http method just to see how html works.

--- Month2_BackEnd/Day10_Feb16/M11A04_ServingHTML/app.js
+++ Month2_BackEnd/Day10_Feb16/M11A04_ServingHTML/app.js
@@ -1,5 +1,19 @@
-const express = require('express');
-const app = express();
+const http = require('http');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 4001;
 
+const handleRequest = (request, response) => {
+    fs.readFile(`${__dirname}/index.html`, (error, data) => {
+        if (error) throw error;
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(data);
+    });
+};
+
+const server = http.createServer(handleRequest);
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on PORT: ${PORT}...`);
+});
+  
