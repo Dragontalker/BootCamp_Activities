b9bcb49b23f10bc25c57454c6339045766fafb09
Richard <richard.yang.tong@gmail.com>
2021-02-17 12:58:55 -0500

    Completed the post handler and successfully tested it with postman.

--- Month2_BackEnd/Day11_Feb17/LearningExpressJS_TraversyMedia/app.js
+++ Month2_BackEnd/Day11_Feb17/LearningExpressJS_TraversyMedia/app.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({extend: false}));
 
 app.use('/api/members', require('./routes/api/members'));
 
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}...`);
--- Month2_BackEnd/Day11_Feb17/LearningExpressJS_TraversyMedia/routes/api/members.js
+++ Month2_BackEnd/Day11_Feb17/LearningExpressJS_TraversyMedia/routes/api/members.js
@@ -14,7 +14,7 @@ router.get('/:id', (req, res) => {
     if (found) {
         res.json(members.filter(member => member.id === parseInt(req.params.id)));
     } else {
-        res.status(404).json({msg: `No member with the id of ${req.params.id}.`});
+        res.status(400).json({msg: `No member with the id of ${req.params.id}.`});
     };
 });
 
@@ -26,6 +26,13 @@ router.post('/', (req, res) => {
         email: req.body.email,
         status: 'active'
     };
+
+    if (!newMember.name || !newMember.email) {
+        return res.status(400).json({msg: 'Please include a name and a email.'})
+    } 
+    
+    members.push(newMember);
+    res.json(members);
 });
 
 module.exports = router;
