a5386511db22ca15b73809273c242dc7ed3708a1
Richard <richard.yang.tong@gmail.com>
2021-02-17 10:26:55 -0500

    Created a get handler to send html upon request.

--- Month2_BackEnd/Day11_Feb17/LearningExpressJS_TraversyMedia/app.js
+++ Month2_BackEnd/Day11_Feb17/LearningExpressJS_TraversyMedia/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>');
+})
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}...`);
