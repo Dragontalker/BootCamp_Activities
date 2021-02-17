002886f21bd938e0aa40892f5eb3a31b35484b0c
Richard <richard.yang.tong@gmail.com>
2021-02-17 10:39:08 -0500

    Added comments to divide each section.

--- Month2_BackEnd/Day11_Feb17/LearningExpressJS_TraversyMedia/app.js
+++ Month2_BackEnd/Day11_Feb17/LearningExpressJS_TraversyMedia/app.js
@@ -1,13 +1,17 @@
+// dependencies
 const express = require('express');
 const path = require('path');
 
+// initializing 
 const app = express();
 
-
+// handlers
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
+
+// creating server
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
