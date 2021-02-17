93e4325d26efcb42453f0138680155af6701cb92
Richard <richard.yang.tong@gmail.com>
2021-02-17 11:49:38 -0500

    Replaced app with router in our handlers.

--- Month2_BackEnd/Day11_Feb17/LearningExpressJS_TraversyMedia/routes/api/members.js
+++ Month2_BackEnd/Day11_Feb17/LearningExpressJS_TraversyMedia/routes/api/members.js
@@ -1,10 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
 // creating an json api for /api/members
-app.get('/api/members', (req, res) => {
-    res.json(members);
-});
+router.get('/api/members', (req, res) => res.json(members));
 
 // getting a single member
-app.get('/api/members/:id', (req, res) => {
+router.get('/api/members/:id', (req, res) => {
     const found = members.some(member => member.id === parseInt(req.params.id));
 
     if (found) {
@@ -13,4 +14,3 @@ app.get('/api/members/:id', (req, res) => {
         res.status(404).json({msg: `No member with the id of ${req.params.id}.`});
     };
 });
-// ===
