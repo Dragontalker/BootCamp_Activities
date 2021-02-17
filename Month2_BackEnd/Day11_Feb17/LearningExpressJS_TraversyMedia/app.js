2e5473e067be3ec7a5bd8a92a761013540115897
Richard <richard.yang.tong@gmail.com>
2021-02-17 11:47:39 -0500

    Created new direcotry called routes to store all the handler moduels .

--- Month2_BackEnd/Day11_Feb17/LearningExpressJS_TraversyMedia/app.js
+++ Month2_BackEnd/Day11_Feb17/LearningExpressJS_TraversyMedia/app.js
@@ -29,21 +29,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 // handlers
 // =====================================================================================
 
-// creating an json api for /api/members
-app.get('/api/members', (req, res) => {
-    res.json(members);
-});
-
-// getting a single member
-app.get('/api/members/:id', (req, res) => {
-    const found = members.some(member => member.id === parseInt(req.params.id));
-
-    if (found) {
-        res.json(members.filter(member => member.id === parseInt(req.params.id)));
-    } else {
-        res.status(404).json({msg: `No member with the id of ${req.params.id}.`});
-    };
-});
 // =====================================================================================
 
 
