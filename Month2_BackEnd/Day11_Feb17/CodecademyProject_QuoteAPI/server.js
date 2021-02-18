19232c4c590dfbed90f314d0f74dcdb55de9c126
Richard <richard.yang.tong@gmail.com>
2021-02-17 20:30:45 -0500

    Completed task 4.

--- Month2_BackEnd/Day11_Feb17/CodecademyProject_QuoteAPI/server.js
+++ Month2_BackEnd/Day11_Feb17/CodecademyProject_QuoteAPI/server.js
@@ -8,7 +8,14 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+// Task 4: Create a GET /api/quotes/random route.
+app.get('/api/quotes/random', (req, res) => {
+    res.send(getRandomElement(quotes));
+});
+
 // Completed task 1-3, server is now live.
 app.listen(PORT, () => {
     console.log(`Server listening at port ${PORT}...`);
 });
+
+
