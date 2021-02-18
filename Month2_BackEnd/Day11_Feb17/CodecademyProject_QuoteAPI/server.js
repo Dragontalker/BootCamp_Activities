abbcb2cb1f11c891df28bc25c869174f7f55f3fb
Richard <richard.yang.tong@gmail.com>
2021-02-17 20:37:36 -0500

    Completed task 5.

--- Month2_BackEnd/Day11_Feb17/CodecademyProject_QuoteAPI/server.js
+++ Month2_BackEnd/Day11_Feb17/CodecademyProject_QuoteAPI/server.js
@@ -13,6 +13,17 @@ app.get('/api/quotes/random', (req, res) => {
     res.send(getRandomElement(quotes));
 });
 
+// Task 5: Creat a GET /api/quotes route with a query string with a person attribute.
+app.get('/api/quotes', (req, res) => {
+    const targetPerson = req.query.person;
+    const found = quotes.some(quote => quote.person === targetPerson);
+    if (found) {
+        res.send(quotes.filter(quote => quote.person === targetPerson));
+    } else {
+        res.status(400).send([]);
+    };
+});
+
 // Completed task 1-3, server is now live.
 app.listen(PORT, () => {
     console.log(`Server listening at port ${PORT}...`);
