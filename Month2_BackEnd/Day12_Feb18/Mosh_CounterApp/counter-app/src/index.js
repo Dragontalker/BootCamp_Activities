92bec7f4b3fe8bb99fa23a8a9a057f45a49d7999
Richard <richard.yang.tong@gmail.com>
2021-02-18 22:01:27 -0500

    Added a new method in inside class.

--- Month2_BackEnd/Day12_Feb18/Mosh_CounterApp/counter-app/src/index.js
+++ Month2_BackEnd/Day12_Feb18/Mosh_CounterApp/counter-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '../node_modules/boostrap/dist/css/bootstrap.css';
+// import '../node_modules/boostrap/dist/css/bootstrap.css';
 import Counter from './components/counter';
 
 ReactDOM.render(
