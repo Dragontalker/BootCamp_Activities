a71c72fc65c61dc2c5a74a020c40f9e09b504bfe
Richard <richard.yang.tong@gmail.com>
2021-02-20 10:35:32 -0500

    Successful connected with our local database.

--- Month2_BackEnd/Day14_Feb20/TraversyMedia_NodeMySQL/app.js
+++ Month2_BackEnd/Day14_Feb20/TraversyMedia_NodeMySQL/app.js
@@ -3,11 +3,14 @@ const mysql = require('mysql');
 
 const app = express();
 
+
 // Create connection
 const db = mysql.createConnection({
     host: 'localhost',
+    port: 3306,
     user: 'root',
-    password: 'sql23248265YT'
+    password: 'sql23248265YT',
+    database: 'favorites_db'
 });
 
 
@@ -20,21 +23,10 @@ db.connect((err) => {
 });
 
 
-// Creat DB
-app.get('/createdb', (req, res) => {
-    let sql = 'CREATE DATABASE nodemysql';
-    db.query(sql, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send('Database created....');
-    });
-});
 
 
 
 // Create a server
-
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
