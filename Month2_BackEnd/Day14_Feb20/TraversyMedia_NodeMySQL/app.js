f1e5a6c0594c42a790efb5ebf9584356672f194a
Richard <richard.yang.tong@gmail.com>
2021-02-20 09:57:54 -0500

    Created a simple server and tested it with nodemon.

--- Month2_BackEnd/Day14_Feb20/TraversyMedia_NodeMySQL/app.js
+++ Month2_BackEnd/Day14_Feb20/TraversyMedia_NodeMySQL/app.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const mysql = require('mysql');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}...`);
+});
