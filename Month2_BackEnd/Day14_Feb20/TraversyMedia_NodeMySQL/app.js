c4c9bb5629d497adb337881f996357efd615bf53
Richard <richard.yang.tong@gmail.com>
2021-02-20 10:00:16 -0500

    Created a mysql connection.

--- Month2_BackEnd/Day14_Feb20/TraversyMedia_NodeMySQL/app.js
+++ Month2_BackEnd/Day14_Feb20/TraversyMedia_NodeMySQL/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const mysql = require('mysql');
 
+// Create connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'sql23248265YT',
+    database: 'nodemysql'
+});
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
