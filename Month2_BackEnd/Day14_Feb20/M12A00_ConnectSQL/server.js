60ca8c0301e35756b00b5c030a8aaf34ab4aac14
Richard <richard.yang.tong@gmail.com>
2021-02-20 10:55:57 -0500

    Added the instruction for mysql workbench.

---
+++ Month2_BackEnd/Day14_Feb20/M12A00_ConnectSQL/server.js
@@ -0,0 +1,14 @@
+  
+/* 
+In mysql workbench:
+===================
+CREATE DATABASE favourites_db;
+USE favourites_db;  
+CREATE TABLE favourite_songs (
+	title VARCHAR(100),
+	artist VARCHAR(100),
+	genre VARCHAR(100),
+	rating INTEGER
+);
+==================
+*/
