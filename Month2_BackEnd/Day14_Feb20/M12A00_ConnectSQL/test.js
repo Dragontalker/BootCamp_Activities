d230937aad119c0ead4e56eb6e7331b1a7fe8265
Richard <richard.yang.tong@gmail.com>
2021-02-20 11:09:15 -0500

    Start an express server.

--- Month2_BackEnd/Day14_Feb20/M12A00_ConnectSQL/test.js
+++ Month2_BackEnd/Day14_Feb20/M12A00_ConnectSQL/test.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const mysql = require('mysql');
 
+const app = express();
+
 const connectDb = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'sql23248265YT',
     database: 'favorites_db'
+});
+
+connectDb.connect((error) => {
+    if (error) {
+        console.log('Error');
+    } else {
+        console.log('Connected');
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}...`);
 });
