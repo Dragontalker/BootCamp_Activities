bf523b925c8e81a311541701c0c47a0c400ff4c0
Richard <richard.yang.tong@gmail.com>
2021-02-20 10:05:35 -0500

    Succefully connected with mysql database.

--- Month2_BackEnd/Day14_Feb20/TraversyMedia_NodeMySQL/app.js
+++ Month2_BackEnd/Day14_Feb20/TraversyMedia_NodeMySQL/app.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const mysql = require('mysql');
 
+const app = express();
+
 // Create connection
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'sql23248265YT',
-    database: 'nodemysql'
+    password: 'sql23248265YT'
 });
 
-const app = express();
+
+// Connect
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('MySQL Connected...');
+});
+
+
+// Creat DB
+app.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE nodemysql';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Database created....');
+    });
+});
+
+
+
+// Create a server
+
 
 const PORT = process.env.PORT || 3000;
 
